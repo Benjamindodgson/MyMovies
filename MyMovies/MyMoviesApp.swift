1f3425798fbad35f1b2ed000c6601f01e7c7a215
Benji Dodgson <benjamindodgson@gmail.com>
2024-10-23 09:07:11 -0500

    Initial Commit

---
+++ MyMovies/MyMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoviesApp.swift
+//  MyMovies
+//
+//  Created by Benjamin Dodgson on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyMoviesTests/MyMoviesTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyMoviesTests.swift
+//  MyMoviesTests
+//
+//  Created by Benjamin Dodgson on 10/23/24.
+//
+
+import Testing
+
+struct MyMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
